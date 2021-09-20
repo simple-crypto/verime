9d795cccb8253e5498879c7727c4ce3d0829d71f
cmomin <charles.momin@uclouvain.be>
2021-09-20 17:37:07 +0200

    rm verilator generate file

--- test/obj_dir/test_main.d
+++
@@ -1,6 +0,0 @@
-test_main.o: ../test_main.cpp ../mylib.h Vtop.h \
- /export/home/cmomin/.local/include/verilated.h \
- /export/home/cmomin/.local/include/verilatedos.h \
- /export/home/cmomin/.local/include/verilated_types.h \
- /export/home/cmomin/.local/include/verilated_funcs.h \
- /export/home/cmomin/.local/include/vltstd/svdpi.h Vtop_top.h Vtop_andg.h
--- test/obj_dir/verilated.d
+++
@@ -1,9 +0,0 @@
-verilated.o: /export/home/cmomin/.local/include/verilated.cpp \
- /export/home/cmomin/.local/include/verilatedos.h \
- /export/home/cmomin/.local/include/verilated_imp.h \
- /export/home/cmomin/.local/include/verilated.h \
- /export/home/cmomin/.local/include/verilated_types.h \
- /export/home/cmomin/.local/include/verilated_funcs.h \
- /export/home/cmomin/.local/include/verilated_syms.h \
- /export/home/cmomin/.local/include/verilated_sym_props.h \
- /export/home/cmomin/.local/include/verilated_config.h
