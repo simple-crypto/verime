b7e8b85bfe55e8a980300ade8fa4b7b35f12e343
cmomin <charles.momin@uclouvain.be>
2021-10-12 12:19:36 +0200

    tested for imbricated generation

--- tests/srcs/and_g.v
+++ tests/srcs/and_g.v
@@ -10,6 +10,16 @@ module and_g
 );
 
 
+(* verilator_me = "test_big_matrix_imbricated" *)
+wire [127:0] mat_big [3:0];
+assign mat_big[0] = 128'd120;
+assign mat_big[1] = 128'd121;
+assign mat_big[2] = 128'd122;
+assign mat_big[3] = 128'd123;
+
+
+
+
 (* verilator_me = "tmp_andg" *)
 wire [W-1:0] tmp;
 assign tmp = a & b;
